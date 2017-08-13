1989083f1c3e9e475766af0226fec551303c01e7
Marc Scheib <marc.scheib@gmx.de>
2017-08-13 18:59:46 +0200

    feat(chips): add input for adding and removing chips

--- src/au-chips.ts
+++ src/au-chips.ts
@@ -3,9 +3,50 @@ import { customElement, inlineView } from 'aurelia-templating';
 @customElement('au-chips')
 @inlineView(`
   <template>
-    <slot></slot>
+    <div>
+      <ul>
+        <li repeat.for="value of values">\${value}</li>
+        <li>
+          <input type="text" keydown.delegate="onKeyDown($event, inputElement)" ref="inputElement">
+        </li>
+      </ul>
+    </div>
   </template>
 `)
 export class AuChips {
+  allowDuplicates: boolean;
+  maxLength: number;
 
+  values: any;
+
+  onKeyDown(event: KeyboardEvent, inputEL: HTMLInputElement): boolean {
+    switch (event.which) {
+      // backspace
+      case 8:
+        if (inputEL.value.length > 0) {
+          return true;
+        } else if (inputEL.value.length === 0 && this.values && this.values.length > 0) {
+          this.values = [...this.values];
+          this.values.pop();
+        }
+        break;
+      // enter
+      case 13:
+        this.addItem(event, inputEL.value);
+        inputEL.value = '';
+        break;
+      default:
+        return true;
+    }
+    return false;
+  }
+
+  addItem(event: Event, item: string): void {
+    this.values = this.values || [];
+    if (item && item.trim().length && (!this.maxLength || this.maxLength > item.length)) {
+      if (this.allowDuplicates || !this.values.includes(item)) {
+        this.values = [...this.values, item];
+      }
+    }
+  }
 }
